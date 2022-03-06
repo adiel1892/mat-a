c9eca5504895d18cf27612a3e904ff7cb042475e
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-06 09:40:33 +0200

    change

--- mat.cpp
+++ mat.cpp
@@ -3,8 +3,20 @@
 #include <stdexcept>
 #include <string>
 using namespace std;
-using namespace ariel;
 
 string ariel::mat(int row,int col,char symbol1,char symbol2){
-    return "not working yet";
+    if(row > 1 && col > 1){
+        return ariel::matPrinterRec(row - 2, col - 2 , symbol1, symbol2);
+    }else if(row > 1){
+        return ariel::matPrinterRec(row - 2, col, symbol1, symbol2);
+    }else if(col > 1){
+        return ariel::matPrinterRec(row, col - 2 , symbol1, symbol2);
+    }else{
+        return ariel::matPrinterRec(row, col , symbol1, symbol2);
+    }
+}
+
+string ariel::matPrinterRec(int row,int col,char symbol1,char symbol2){
+    
+
 }
--- mat.hpp
+++ mat.hpp
@@ -2,4 +2,5 @@
 using namespace std;
 namespace ariel{
     string mat(int col, int row, char symbol1, char symbol2);
+    String matPrinterRec(int col, int row, char symbol1, char symbol2);
 }
